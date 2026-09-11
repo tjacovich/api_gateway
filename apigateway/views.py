d1f38a8a228c063975a19a5bb52f4d0d8bd182df
tjacovich <tjacovich@cfa.harvard.edu>
2026-09-11 19:05:42 -0400

    Add environment variable for toggling disposable email blocking.

--- apigateway/views.py
+++ apigateway/views.py
@@ -221,10 +221,11 @@ class UserManagementView(Resource):
             return {"message": "success"}, 200
 
         try:
-            email_domain = params.email.split('@')[1]
-            if email_domain in blocklist:
-                current_app.logger.warning("User {} attempting to register with blocked email domain.".format(params.email))
-                raise ValueError("Invalid email domain: {}. We do not allow account registration with temporary email services.".format(email_domain))
+            if current_app.config.get("BLOCK_DISPOSABLE_EMAIL_DOMAINS", False):
+                email_domain = params.email.split('@')[1]
+                if email_domain in blocklist:
+                    current_app.logger.warning("User {} attempting to register with blocked email domain.".format(params.email))
+                    raise ValueError("Invalid email domain: {}. We do not allow account registration with temporary email services.".format(email_domain))
             
             user: User = extensions.security_service.create_user(
                 given_name=params.given_name,
