46749ea12d99db90d328bcc471a0673835e7a5f6
tjacovich <tjacovich@cfa.harvard.edu>
2026-09-11 19:00:25 -0400

    update UserManagementView to block temporary domains.

--- apigateway/views.py
+++ apigateway/views.py
@@ -4,6 +4,7 @@ import json
 from copy import copy
 from datetime import datetime
 from urllib.parse import unquote
+from disposable_email_domains import blocklist
 
 import requests
 from authlib.integrations.flask_oauth2 import current_token
@@ -220,6 +221,11 @@ class UserManagementView(Resource):
             return {"message": "success"}, 200
 
         try:
+            email_domain = params.email.split('@')[1]
+            if email_domain in blocklist:
+                current_app.logger.warning("User {} attempting to register with blocked email domain.".format(params.email))
+                raise ValueError("Invalid email domain: {}. We do not allow account registration with temporary email services.".format(email_domain))
+            
             user: User = extensions.security_service.create_user(
                 given_name=params.given_name,
                 family_name=params.family_name,
